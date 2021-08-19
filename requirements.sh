68a8b3d0a172202fa971b902fd2a735ffb8dbe69
White_Fox <61945407+Grig773@users.noreply.github.com>
2021-08-19 15:03:36 +0400

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+echo ""
+apt-get update -y
+apt-get upgrade -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install mpv -y
+pkg install ruby -y
+gem install lolcat
+pkg install git -y
+pkg install figlet -y
+echo ""
